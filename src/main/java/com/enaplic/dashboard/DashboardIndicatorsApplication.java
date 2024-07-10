6c66bff9b16449fb940b26684abef690c3684caf
Eliezer Moraes Silva <eliezer.moraes@outlook.com>
2024-07-10 15:42:42 -0300

    first commit

---
+++ src/main/java/com/enaplic/dashboard/DashboardIndicatorsApplication.java
@@ -0,0 +1,13 @@
+package com.enaplic.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardIndicatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardIndicatorsApplication.class, args);
+	}
+
+}
